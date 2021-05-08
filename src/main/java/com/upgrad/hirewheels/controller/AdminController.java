4cddb4365db6f7830a3672e61b9844249b92676e
Moulina <moulinahzr@gmail.com>
2021-05-09 00:20:40 +0530

    CheckPoint-03-Mini-Project-PUTMethod

--- src/main/java/com/upgrad/hirewheels/controller/AdminController.java
+++ src/main/java/com/upgrad/hirewheels/controller/AdminController.java
@@ -8,10 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/hirewheels/v1")
@@ -43,5 +40,28 @@ public class AdminController {
 
         //Returning the newly created vehicle
         return new ResponseEntity<>(savedVehicleDTO, HttpStatus.CREATED);
+    }
+
+    /**
+     * +++++++++++ CHECKPOINT - 03 +++++++++++++
+     * This is HTTP PUT Method which is responsible
+     * for updating a already present vehicle in the database.
+     */
+    @PutMapping(value = "/vehicles/{id}", consumes = MediaType.APPLICATION_JSON_VALUE ,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<VehicleDTO> updateVehicle(@PathVariable(value = "id") int id,
+                                                    @RequestBody VehicleDTO vehicleDTO){
+
+        //Map Vehicle DTO to Vehicle Entity
+        Vehicle newVehicle = modelMapper.map(vehicleDTO,Vehicle.class);
+
+        //Call Admin service to change availability status a existing Vehicle by vehicle id
+        Vehicle updatedVehicle = adminService.changeAvailability(id,newVehicle.getAvailabilityStatus());
+
+        //Map Vehicle Entity to Vehicle DTO
+        VehicleDTO updatedVehicleDTO = modelMapper.map(updatedVehicle,VehicleDTO.class);
+
+        //Returning the newly created vehicle
+        return new ResponseEntity<>(updatedVehicleDTO, HttpStatus.ACCEPTED);
     }
 }
